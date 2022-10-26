5674a257d1acf65f005d88fada6c7a6d1a8075ca
ratulislam <iratul010@gmail.com>
2022-10-26 19:16:07 +0600

    course data check

--- index.js
+++ index.js
@@ -7,8 +7,23 @@ app.get("/", (req, res) => {
   res.send("Booster Courses API is Running!");
 });
 const categories = require("./data/categories.json");
+const courses = require("./data/courses.json");
 app.get("/course-categories", (req, res) => {
   res.send(categories);
+});
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  if (id === "08") {
+    res.send(courses);
+  } else {
+    const category_course = courses.filter((category) => category.category_id === id);
+    res.send(category_course);
+  }
+});
+app.get("/course/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedCourse = courses.find((course) => course.id === id);
+  res.send(selectedCourse);
 });
 app.listen(port, () => {
   console.log("Booster Courses Api Running on Port: ", port);
