0c88e2cdcb5a9ce4342a55d944fd6319e5370002
ratulislam <iratul010@gmail.com>
2022-10-28 00:11:17 +0600

    server-set-delay

--- index.js
+++ index.js
@@ -11,18 +11,21 @@ const courses = require("./data/courses.json");
 app.get("/course-categories", (req, res) => {
   res.send(categories);
 });
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
 app.get("/category/:id", (req, res) => {
   const id = req.params.id;
   if (id === "08") {
     res.send(courses);
   } else {
-    const category_course = courses.filter((category) => category.category_id === id);
+    const category_course = courses.filter((course) => course.category_id === id);
     res.send(category_course);
   }
 });
 app.get("/course/:id", (req, res) => {
   const id = req.params.id;
-  const selectedCourse = courses.find((course) => course.id === id);
+  const selectedCourse = courses.find((course) => course._id === id);
   res.send(selectedCourse);
 });
 app.listen(port, () => {
